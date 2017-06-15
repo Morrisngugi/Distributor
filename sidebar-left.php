b8504fd16edefb302da87d8ae6ec4e20226e0756
Morris <ngugimorry@gmail.com>
2017-06-15 13:10:49 +0300

    Change file names

--- sidebar-left.php
+++ sidebar-left.php
@@ -2,148 +2,79 @@
         <div class="content">
           <div class="am-logo"></div>
           <ul class="sidebar-elements">
-            <li class="parent"><a href="#"><i class="icon s7-monitor"></i><span>Dashboard</span></a>
+            <li class="active" ><a href="index.php"><i class="icon s7-monitor"></i><span>Dashboard</span></a>
+             
+            </li>
+            <li class="parent"><a href="#"><i class="icon s7-diamond"></i><span>Master Data</span></a>
               <ul class="sub-menu">
-                <li class="active"><a href="index.html">Version 1</a>
+                <li><a href="users.php">Users</a>
                 </li>
-                <li><a href="dashboard2.html">Version 2</a>
+                <li><a href="products.php">Product</a>
                 </li>
-                <li><a href="dashboard3.html"><span class="label label-primary pull-right">New</span>Version 3</a>
+                <li><a href="brands.php">Brands</a>
                 </li>
-                <li><a href="dashboard4.html"><span class="label label-primary pull-right">New</span>Version 4</a>
+                <li><a href="packiging.php">Packaging</a>
+                </li>
+                <li><a href="product-type.php">Product Type</a>
+                </li>
+                <li><a href="lots.php">Lots</a>
+                </li>
+                <li><a href="sub-brands.php">Sub-Brands</a>
+                </li>
+                <li><a href="packaging-type.php">Packaging Type</a>
+                </li>
+                <li><a href="unit-of-measure.php">Unit of Measure</a>
+                </li>
+                <li><a href="vat-rates.php">VAT Rates</a>
+                </li>
+                <li><a href="custom-fields.php">Custom Fields</a>
+                </li>
+                <li><a href="metrics-definition.php">Metrics Definition</a>
+                </li>
+                <li><a href="pricing.php">Pricing</a>
+                </li>
+                <li><a href="clusters.php">Clusters</a>
+                </li>
+                <li><a href="entries.php">Entities</a>
                 </li>
               </ul>
             </li>
-            <li class="parent"><a href="#"><i class="icon s7-diamond"></i><span>UI Elements</span></a>
+            <li class="parent"><a href="#"><i class="icon s7-graph"></i><span>Transactions</span></a>
               <ul class="sub-menu">
-                <li><a href="ui-general.html">General</a>
+                <li><a href="charts-flot.php">Flot</a>
                 </li>
-                <li><a href="ui-alerts.html">Alerts</a>
+                <li><a href="charts-morris.php">Morris.js</a>
                 </li>
-                <li><a href="ui-panels.html">Panels</a>
-                </li>
-                <li><a href="ui-buttons.html">Buttons</a>
-                </li>
-                <li><a href="ui-modals.html">Modals</a>
-                </li>
-                <li><a href="ui-notifications.html">Notifications</a>
-                </li>
-                <li><a href="ui-icons.html">Icons</a>
-                </li>
-                <li><a href="ui-grid.html">Grid</a>
-                </li>
-                <li><a href="ui-tabs-accordions.html">Tabs &amp; Accordions</a>
-                </li>
-                <li><a href="ui-chat.html"><span class="label label-primary pull-right">New</span>Chat Component</a>
-                </li>
-                <li><a href="ui-nestable-lists.html">Nestable Lists</a>
-                </li>
-                <li><a href="ui-typography.html"><span class="label label-primary pull-right">New</span>Typography</a>
+                <li><a href="charts-sparkline.php">Sparklines</a>
                 </li>
               </ul>
             </li>
-            <li class="parent"><a href="#"><i class="icon s7-graph"></i><span>Charts</span></a>
+            <li class="parent"><a href="#"><i class="icon s7-ribbon"></i><span>Settings</span></a>
               <ul class="sub-menu">
-                <li><a href="charts-flot.html">Flot</a>
+                <li><a href="form-elements.php">Elements</a>
                 </li>
-                <li><a href="charts-morris.html">Morris.js</a>
+                <li><a href="form-validation.php">Validation</a>
                 </li>
-                <li><a href="charts-sparkline.html">Sparklines</a>
+                <li><a href="form-spinner.php"><span class="label label-primary pull-right">New</span>Spinner</a>
+                </li>
+                <li><a href="form-wizard.php">Wizard</a>
+                </li>
+                <li><a href="form-masks.php">Input Masks</a>
+                </li>
+                <li><a href="form-wysiwyg.php">WYSIWYG Editor</a>
+                </li>
+                <li><a href="form-upload.php">Multi Upload</a>
                 </li>
               </ul>
             </li>
-            <li class="parent"><a href="#"><i class="icon s7-ribbon"></i><span>Forms</span></a>
+            <li class="parent"><a href="#"><i class="icon s7-box2"></i><span>Reports</span></a>
               <ul class="sub-menu">
-                <li><a href="form-elements.html">Elements</a>
+                <li><a href="tables-general.php">General</a>
                 </li>
-                <li><a href="form-validation.html">Validation</a>
-                </li>
-                <li><a href="form-spinner.html"><span class="label label-primary pull-right">New</span>Spinner</a>
-                </li>
-                <li><a href="form-wizard.html">Wizard</a>
-                </li>
-                <li><a href="form-masks.html">Input Masks</a>
-                </li>
-                <li><a href="form-wysiwyg.html">WYSIWYG Editor</a>
-                </li>
-                <li><a href="form-upload.html">Multi Upload</a>
+                <li><a href="tables-datatables.php">Data Tables</a>
                 </li>
               </ul>
             </li>
-            <li class="parent"><a href="#"><i class="icon s7-box2"></i><span>Tables</span></a>
-              <ul class="sub-menu">
-                <li><a href="tables-general.html">General</a>
-                </li>
-                <li><a href="tables-datatables.html">Data Tables</a>
-                </li>
-              </ul>
-            </li>
-            <li class="parent"><a href="#"><i class="icon s7-note2"></i><span>Pages</span></a>
-              <ul class="sub-menu">
-                <li><a href="pages-blank.html">Blank Page</a>
-                </li>
-                <li><a href="pages-blank-header.html">Blank Page Header</a>
-                </li>
-                <li><a href="pages-login.html">Login</a>
-                </li>
-                <li><a href="pages-404.html">404 Page</a>
-                </li>
-                <li><a href="pages-sign-up.html">Sign Up</a>
-                </li>
-                <li><a href="pages-forgot-password.html">Forgot Password</a>
-                </li>
-                <li><a href="pages-profile.html">Profile</a>
-                </li>
-                <li><a href="pages-calendar.html">Calendar</a>
-                </li>
-                <li><a href="pages-gallery.html">Gallery</a>
-                </li>
-                <li><a href="pages-pricing-tables.html"><span class="label label-primary pull-right">New</span>Pricing Tables</a>
-                </li>
-                <li><a href="pages-timeline.html"><span class="label label-primary pull-right">New</span>Timeline</a>
-                </li>
-                <li><a href="pages-timeline2.html"><span class="label label-primary pull-right">New</span>Timeline v2</a>
-                </li>
-                <li><a href="pages-invoice.html"><span class="label label-primary pull-right">New</span>Invoice</a>
-                </li>
-              </ul>
-            </li>
-            <li class="parent"><a href="#"><i class="icon s7-mail"></i><span>Email</span></a>
-              <ul class="sub-menu">
-                <li><a href="email-inbox.html">Inbox</a>
-                </li>
-                <li><a href="email-read.html">Email Detail</a>
-                </li>
-                <li><a href="email-compose.html">Email Compose</a>
-                </li>
-              </ul>
-            </li>
-            <li class="parent"><a href="#"><i class="icon s7-browser"></i><span>Layouts</span></a>
-              <ul class="sub-menu">
-                <li><a href="layouts-nosidebar-left.html">Without Left Sidebar</a>
-                </li>
-                <li><a href="layouts-nosidebar-right.html">Without Right Sidebar</a>
-                </li>
-                <li><a href="layouts-nosidebars.html">Without Both Sidebars</a>
-                </li>
-                <li><a href="layouts-white-header.html">White Header</a>
-                </li>
-                <li><a href="layouts-sticky-footer.html"><span class="label label-primary pull-right">New</span>Sticky Footer</a>
-                </li>
-                <li><a href="layouts-fixed-sidebar.html">Fixed Left Sidebar</a>
-                </li>
-                <li><a href="layouts-left-sidebar-click.html"><span class="label label-primary pull-right">New</span>Open left sidebar on click</a>
-                </li>
-                <li><a href="pages-blank-aside.html">Page Aside</a>
-                </li>
-              </ul>
-            </li>
-            <li class="parent"><a href="#"><i class="icon s7-map-marker"></i><span>Maps</span></a>
-              <ul class="sub-menu">
-                <li><a href="maps-google.html">Google Maps</a>
-                </li>
-                <li><a href="maps-vector.html">Vector Maps</a>
-                </li>
               </ul>
             </li>
           </ul>
