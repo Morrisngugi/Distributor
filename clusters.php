7157031a5eb6fdee24a813fa0f2d96cc994a978a
Jorje65 <gexxmwangi@gmail.com>
2017-06-22 11:22:51 +0300

    Cluster form changes

---
+++ clusters.php
@@ -0,0 +1,119 @@
+<!-- Begin Header -->
+<?php include 'header.php';?>
+<!-- End Header -->
+
+<!-- Begin Left Sidebar -->
+<?php include 'sidebar-left.php';?>
+<!-- End Left Sidebar -->
+
+
+<!-- Begin Content  -->
+<div class="am-content">
+        <div class="page-head">
+          <h2>Clusters</h2>
+          <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Master Data</a></li>
+            <li class="active">Clusters</li>
+          </ol>
+        </div>
+        <div class="main-content">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="widget widget-fullwidth widget-small">
+                <div class="widget-head">
+                  <div class="tools"><span class="icon s7-upload"></span><span class="icon s7-edit"></span><span class="icon s7-close"></span></div>
+                  <div class="title">Clusters</div>
+                  
+                
+                </br>
+                <div class="form-group">
+                   <a href="addclusters.php"
+                          <div class="col-md-4 col-md-offset-11">
+                              <Button class="btn btn-primary" name="btn"  > Add</button>
+                          </div></a>
+                    </div>
+                <table class="table table-fw-widget">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>ID</th>
+                      <th>Code</th>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr class="odd gradeX">
+                      <td>Trident</td>
+                      <td>
+                        Internet
+                        Explorer 4.0
+                      </td>
+                      <td>Win 95+</td>
+                      <td class="center"> 4</td>
+                      
+                      <td class="center"></td>
+                    </tr>
+                    <tr class="even gradeC">
+                      <td>Trident</td>
+                      <td>
+                        Internet
+                        Explorer 5.0
+                      </td>
+                      <td>Win 95+</td>
+                      <td class="center">5</td>
+                      <td class="center">C</td>
+                      <td class="center"></td>
+                    </tr>
+                    <tr class="odd gradeA">
+                      <td>Trident</td>
+                      <td>
+                        Internet
+                        Explorer 5.5
+                      </td>
+                      <td>Win 95+</td>
+                      <td class="center">5.5</td>
+                      <td class="center">A</td>
+                      <td class="center"></td>
+                    </tr>
+                    <tr class="even gradeA">
+                      <td>Trident</td>
+                      <td>
+                        Internet
+                        Explorer 6
+                      </td>
+                      <td>Win 98+</td>
+                      <td class="center">6</td>
+                      <td class="center">A</td>
+                      <td class="center"></td>
+                    </tr>
+                    <tr class="odd gradeA">
+                      <td>Trident</td>
+                      <td>Internet Explorer 7</td>
+                      <td>Win XP SP2+</td>
+                      <td class="center">7</td>
+                      <td class="center">A</td>
+                      <td class="center"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      
+<!-- End Content -->
+
+
+<!-- Right Sidebar -->
+<?php include 'sidebar-right.php';?>
+<!-- End Right Sidebar -->
+
+</div>
+
+<!-- Begin Footer -->
+<?php include 'footer.php';?>
+<!-- End Footer -->
